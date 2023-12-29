5c2b04e726abcc55297f71217f521260432bfee2
jv-vogler <jvsvogler@gmail.com>
2023-12-29 15:27:53 -0300

    feat: add initial todo list

---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,36 @@
+import { useContext } from 'react'
+
+import { TodosContext } from '../context/TodosProvider'
+
+export const useTodos = () => {
+  const value = useContext(TodosContext)
+
+  if (value === undefined) {
+    throw new Error('You forgot the TodosContext Provider')
+  }
+
+  const { todos, setTodos } = value
+
+  const addTodo = (todoText: string) => {
+    if (todoText.trim() !== '') {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text: todoText,
+          completed: false,
+        },
+      ])
+    }
+  }
+
+  const toggleTodo = (id: string) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)))
+  }
+
+  const deleteTodo = (id: string) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  }
+
+  return { todos, addTodo, toggleTodo, deleteTodo }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.tsx'
+import { TodosProvider } from './context/TodosProvider.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <TodosProvider>
+      <App />
+    </TodosProvider>
+  </React.StrictMode>
 )
