23435210427e6db08b045ac517441cc8effbe27f
jv-vogler <jvsvogler@gmail.com>
2023-12-29 14:41:11 -0300

    batman: this commit has no parents

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex w-screen flex-col items-center justify-center p-2">
+        <div className="container flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold text-blue-500">Command Pattern</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
