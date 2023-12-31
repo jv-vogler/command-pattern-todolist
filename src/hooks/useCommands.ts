e57b66c651d69afd61499c82f1a7f453abed29ad
jv-vogler <jvsvogler@gmail.com>
2023-12-31 01:00:08 -0300

    feat: memoize useCommands functions

--- src/hooks/useCommands.ts
+++ src/hooks/useCommands.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 import { Command } from '../domain/entities/Command'
 
@@ -6,27 +6,30 @@ export const useCommands = () => {
   const [commandHistory, setCommandHistory] = useState<Array<Command>>([])
   const [historyPointer, setHistoryPointer] = useState<number>(-1)
 
-  const executeCommand = (command: Command) => {
-    command.execute()
-    setCommandHistory((prevCommands) => [...prevCommands.slice(0, historyPointer + 1), command])
-    setHistoryPointer((prevPointer) => prevPointer + 1)
-  }
+  const executeCommand = useCallback(
+    (command: Command) => {
+      command.execute()
+      setCommandHistory((prevCommands) => [...prevCommands.slice(0, historyPointer + 1), command])
+      setHistoryPointer((prevPointer) => prevPointer + 1)
+    },
+    [historyPointer, setCommandHistory, setHistoryPointer]
+  )
 
-  const undoCommand = () => {
+  const undoCommand = useCallback(() => {
     if (historyPointer < 0) return
 
     const lastCommand = commandHistory[historyPointer]
     setHistoryPointer((prevPointer) => prevPointer - 1)
     lastCommand.undo()
-  }
+  }, [historyPointer, commandHistory, setHistoryPointer])
 
-  const redoCommand = () => {
+  const redoCommand = useCallback(() => {
     if (historyPointer >= commandHistory.length - 1) return
 
     const nextCommand = commandHistory[historyPointer + 1]
     setHistoryPointer((prevPointer) => prevPointer + 1)
     nextCommand.execute()
-  }
+  }, [historyPointer, commandHistory, setHistoryPointer])
 
   return { executeCommand, undoCommand, redoCommand }
 }
